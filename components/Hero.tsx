b76ea3f22bb92d17d4eafc62746d40d0a0739b0a
nchahal3 <nabi.chahal@gmail.com>
2026-06-07 16:53:38 -0600

    Update Watch Free Training link to free Discord channel

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -103,7 +103,7 @@ export default function Hero() {
             Join the Community
           </a>
           <a
-            href="https://discord.gg/TvhhdGtU"
+            href="https://discord.gg/QShqUxBK"
             target="_blank"
             rel="noopener noreferrer"
             className="btn-outline px-8 py-4 text-lg"
